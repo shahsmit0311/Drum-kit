378a3939f961f411f1ed1299058b44e992309388
shahsmit0311 <155864079+shahsmit0311@users.noreply.github.com>
2024-01-25 12:48:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ document.addEventListener("keydown",function(event){
 
 function playSound(key){
     switch (key) {
+
+        // lowerCase letters
         case "w":
             var tom1=new Audio("./sounds/tom-1.mp3");
             tom1.play();
@@ -50,6 +52,42 @@ function playSound(key){
             kick.play();
             break;
 
+        //upperCase letters
+        case "W":
+            var tom1=new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "A":
+            var tom2=new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "S":
+            var tom3=new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "D":
+            var tom4=new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "J":
+            var snare=new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "K":
+            var crash=new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;  
+              
+        case "L":
+            var kick=new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+            
         default: console.log(key)
             break;
     }
